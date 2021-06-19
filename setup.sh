4b5b39ed9e56bf46790f97954988dd2c50302370
Michael Bao <68862733+tcmmichaelb139@users.noreply.github.com>
2021-06-19 15:50:24 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 cd $HOME/dotfiles
-cp .profile $HOME/.profile
+mv .profile $HOME/.profile
 echo "Copied .profile"
 
 mkdir .config
@@ -24,24 +24,24 @@ cd i3blocks
 git clone https://github.com/vivien/i3blocks-contrib
 
 cd $HOME/dotfiles/.config/i3
-cp config $HOME/.config/i3/config
+mv config $HOME/.config/i3/config
 echo "Copied i3 config"
 
 cd $HOME/dotfiles/.config/i3blocks
-cp config $HOME/.config/i3blocks/config
+mv config $HOME/.config/i3blocks/config
 echo "Copied i3blocks config"
 
 cd $HOME/dotfiles/.config/zsh/
-cp .zshrc $HOME/.config/zsh/.zshrc
+mv .zshrc $HOME/.config/zsh/.zshrc
 
 cd $HOME/dotfiles/.config/nvim/
-cp init.vim $HOME/.config/nvim/init.vim
+mv init.vim $HOME/.config/nvim/init.vim
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 echo "Copied init.vim and installed vim-plug"
 
 cd $HOME/dotfiles/.config/alacritty/
-cp alacritty.yml $HOME/.config/alacritty/alacritty.yml
+mv alacritty.yml $HOME/.config/alacritty/alacritty.yml
 echo "Copied alacritty config"
 
 echo "Installing i3block dependencies"
